3bd1c4cd6ffc567d88bda48e43644009ad824f10
rahulsai1999 <noneuser2183@gmail.com>
2020-01-11 15:17:23 +0530

    fade animation done

--- src/components/rightsec.jsx
+++ src/components/rightsec.jsx
@@ -5,20 +5,20 @@ import {
   prevButton as PButton,
   nextButton as NButton
 } from "./buttons";
-import { Row, Col } from "react-bootstrap";
+import { Row, Col, Container } from "react-bootstrap";
 
 const animParam = {
   first: {
-    open: { opacity: 1 },
-    closed: { opacity: 0 }
+    open: { display: "block", opacity: 1, x: "27%" },
+    closed: { display: "none", opacity: 0 }
   },
   second: {
-    open: { opacity: 1, y: "-90%" },
-    closed: { opacity: 0 }
+    open: { display: "block", opacity: 1 },
+    closed: { display: "none", opacity: 0 }
   },
   third: {
-    open: { opacity: 1, y: "-180%" },
-    closed: { opacity: 0 }
+    open: { display: "block", opacity: 1 },
+    closed: { display: "none", opacity: 0, x: "-25%" }
   }
 };
 
@@ -36,64 +36,69 @@ const RightSec = props => {
           backgroundColor: isOpen ? "#657991" : "#FFFFFF",
           opacity: isOpen ? 1 : 0,
           height: 700,
-          width: 800
+          width: 900
         }}
       >
-        <Row>
-          <Col>
-            <CButton
-              onClick={() => {
-                stateHook(!isOpen);
-              }}
-            >
-              Close
-            </CButton>
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <motion.div
-              animate={carState == 1 ? "open" : "closed"}
-              variants={animParam.first}
-              transition={{ velocity: 0.5 }}
-            >
-              <img src="img/card1.png" alt="card1" />
-            </motion.div>
-            <motion.div
-              animate={carState == 2 ? "open" : "closed"}
-              variants={animParam.second}
-              transition={{ velocity: 0.5 }}
-            >
-              <img src="img/card2.png" alt="card1" />
-            </motion.div>
-            <motion.div
-              animate={carState == 3 ? "open" : "closed"}
-              variants={animParam.third}
-              transition={{ velocity: 0.5 }}
-            >
-              <img src="img/card3.png" alt="card1" />
-            </motion.div>
-          </Col>
-        </Row>
-
-        <Row>
-          <Col>
-            <PButton
-              style={carState == 1 ? { display: "none" } : null}
-              onClick={() => {
-                setCarState(prev => prev - 1);
-              }}
-            />
-          </Col>
-          <Col>
-            <NButton
-              style={carState == 3 ? { display: "none" } : null}
-              onClick={() => {
-                setCarState(prev => prev + 1);
-              }}
-            />
-          </Col>
-        </Row>
+        <Container>
+          <Row>
+            <Col>
+              <CButton
+                onClick={() => {
+                  stateHook(!isOpen);
+                }}
+              >
+                Close
+              </CButton>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <motion.div
+                animate={carState === 1 ? "open" : "closed"}
+                variants={animParam.first}
+                transition={{ velocity: 0.5 }}
+              >
+                <img src="img/card1.png" height={410} alt="card1" />
+              </motion.div>
+            </Col>
+            <Col>
+              <motion.div
+                animate={carState === 2 ? "open" : "closed"}
+                variants={animParam.second}
+                transition={{ velocity: 0.5 }}
+              >
+                <img src="img/card2.png" height={410} alt="card1" />
+              </motion.div>
+            </Col>
+            <Col>
+              <motion.div
+                animate={carState === 3 ? "open" : "closed"}
+                variants={animParam.third}
+                transition={{ velocity: 0.5 }}
+              >
+                <img src="img/card3.png" height={410} alt="card1" />
+              </motion.div>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <PButton
+                style={carState === 1 ? { display: "none" } : null}
+                onClick={() => {
+                  setCarState(prev => prev - 1);
+                }}
+              />
+            </Col>
+            <Col>
+              <NButton
+                style={carState === 3 ? { display: "none" } : null}
+                onClick={() => {
+                  setCarState(prev => prev + 1);
+                }}
+              />
+            </Col>
+          </Row>
+        </Container>
       </div>
     </motion.div>
   );
